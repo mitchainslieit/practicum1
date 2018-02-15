17f1cbb1d291a0ece6b296acceeed12666e2ba68
DeanBaguisi <2163907@slu.edu.ph>
2018-02-15 11:52:23 +0800

    Added Login html

---
+++ Login - Copy/login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="style.css">
+		<meta charset="UTF-8">
+		<title>School of Nursing</title>
+	</head>
+
+	<body>
+		<center>
+			<div id="banner">
+				<img src="logo.png" alt="SON Logo" height="150" width="150">
+			</div>
+			<div id = "loginpage">
+			<h1> Saint Louis University </h1>
+			<h1> School of Nursing </h1>
+				<div id = "login-form">
+					  <form>
+							<div id = "username">
+								<input type="text" name="user" placeholder="Username"><br>
+							</div>
+							<div id="password">
+								<input type="password" name="pass" placeholder="Password"><br>
+							</div>
+							<div id="login">
+								<input type="submit" name="login" class="login login-submit" value="Login">
+							</div>
+					  </form>					
+				</div>
+			</div>
+		</center>
+		
+	</body>
+	
+</html>
